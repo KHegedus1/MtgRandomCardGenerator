d04a7477629008d53be065423fa78dd395742c53
KHegedus1 <kico.kiki@gmail.com>
2022-10-05 10:50:13 +0200

    next card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import {FaArrowRight} from "react-icons/fa";
 
 function App() {
   //js kod
   return (
     <>
     <div id ="mainWrap">
-    <div id="card">
-    </div>
+    <div id="card"></div>
+    <div id="nextCard">NEXT CARD {FaArrowRight}</div>
     </div>
     </>
   );
